8c3add052b3ad8352159382bbc21ab7a4f263353
e <401840614@qq.com>
2017-06-07 21:20:14 +0800

    修改遮罩层样式
    以上

--- src/components/captcha/index.vue
+++ src/components/captcha/index.vue
@@ -1,13 +1,14 @@
 <template>
   <!--:class="{'input_captchaT':registerData.captcha}"-->
-  <div  :class="{'input_captchaT':captchaState,'label-error':captchaError}" class="div_input input_top  input_captcha" ref="Captcha"> 
-    <input type="captcha" placeholder="验证码">
+  <div  :class="{'input_captchaT':captchaState,'label-error':captchaError}" class="div_input input_top  input_captcha" ref="imageCaptcha" > 
+    <input type="captcha" placeholder="验证码" v-model="captchaData">
     <label>{{captchaError}}</label>
     <span class="captcha"><img :src='captchaAdress' alt="验证码" @click='changeCaptch'></span>
   </div>
 </template>
 
 <script>
+  import { bus } from "../login/bus";
   import { apiDomain } from "@/common/js/public.js";
 
   export default {
@@ -19,6 +20,7 @@
     props: ['captchaError'],
     data() {
       return {
+        captchaData:'',
         captchaState:false,
         captcha: false,
         captchaAdress: `${apiDomain}/captcha?${new Date().getTime()}`,
@@ -28,6 +30,11 @@
       changeCaptch() {
         this.captchaAdress = `${apiDomain}/captcha?${new Date().getTime()}`
       },
+    },
+    watch:{
+      captchaData(d){
+        bus.$emit('captchaData', d)
+      }
     }
   }
 </script>
