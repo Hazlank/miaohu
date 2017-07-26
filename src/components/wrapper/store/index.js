aa520f66cc6a167c379f756f13624457bae42b17
-A- <1157988103@qq.com>
2017-07-26 11:40:04 +0800

    三元没改对已重新修改

--- src/components/wrapper/store/index.js
+++ src/components/wrapper/store/index.js
@@ -6,27 +6,31 @@ import * as actions from './actions';
 Vue.use(Vuex);
 
 let state = {
-    wrapper: false,
-    wrapperRegister: false,
-    wrapperQuestion: false,
+  wrapper: false,
+  wrapperRegister: false,
+  wrapperQuestion: false,
 
 };
 
 
 const store = new Vuex.Store({
-    state,
-    getters,
-    actions,
-    mutations: {
-        wrapperDisplay(state, type) {
-            let html = document.getElementsByTagName('html')[0]
-            state[type] = true;
-            if(state.wrapper)
-              state.wrapper = false, html.style.overflow = 'initial'
-            else
-              state.wrapper = true, html.style.overflow = 'hidden'
-        },
-    }
+  state,
+  getters,
+  actions,
+  mutations: {
+    wrapperDisplay(state, type) {
+      let html = document.getElementsByTagName('html')[0]
+      state[type] = true;
+      if (state.wrapper) {
+        state.wrapper = false;
+        html.style.overflow = 'initial'
+      }
+      else {
+        state.wrapper = true;
+        html.style.overflow = 'hidden'
+      }
+    },
+  }
 });
 
 export default store;
